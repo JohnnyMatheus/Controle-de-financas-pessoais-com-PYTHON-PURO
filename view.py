0e5f6ae412d50dd51d4b9780c4ac7d737ee37545
Johnny Matheus <alunotioficial@gmail.com>
2025-02-16 01:02:20 -0300

    Feito manipulação da conta

--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-from model import Conta,engine
+from model import Conta,engine,Bancos, Status
 from sqlmodel import Session,select
 
 def criar_conta(conta: Conta):
@@ -27,11 +27,33 @@ def desativar_conta(id):
     with Session(engine) as session:
         statement = select(Conta).where(Conta.id ==id)
         conta = session.exec(statement).first()
-        print(conta.banco)
+        if conta.valor > 0:
+            raise ValueError('Essa conta ainda possui saldo!')
+        conta.status = Status.INATIVO
+        session.commit()
 
+def transferir_saldo(id_conta_saida, id_conta_entrada,valor):
+    with Session(engine) as session:
+        statement = select(Conta).where(Conta.id==id_conta_saida)
+        conta_saida = session.exec(statement).first()
+        if conta_saida.valor < valor:
+            raise ValueError('Saldo insuficiente')
+        
+        statement = select(Conta).where(Conta.id==id_conta_entrada)
+        conta_entrada = session.exec(statement).first()
+        
+        conta_saida.valor -= valor
+        conta_entrada.valor += valor
+        session.commit()
+        
+        #print(conta_saida)
 
-#conta = Conta(valor=10, banco=Bancos.INTER)
+#conta = Conta(valor=0, banco=Bancos.INTER)
+#conta = Conta(valor=10, banco=Bancos.SANTANDER)
+#conta = Conta(valor=10, banco=Bancos.NUBANK)
+
 #criar_conta(conta)
-desativar_conta(1)
+#desativar_conta(1)
 
-print(listar_contas())
+transferir_saldo(2,3,1)
+#print(listar_contas())
